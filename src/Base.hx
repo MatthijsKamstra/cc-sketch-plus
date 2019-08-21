00661da14d6a24e878676617aaabad33dcb46d50
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-08-21 11:17:05 +0200

    add id and name of class

--- src/Base.hx
+++ src/Base.hx
@@ -1,4 +1,25 @@
+package;
+
 class Base {
+	@:isVar public var id(get, set):String;
+
+	// ____________________________________ getter/setter ____________________________________
+
+	function get_id():String {
+		return id;
+	}
+
+	function set_id(value:String):String {
+		return id = value;
+	}
+
+	// ____________________________________ toString ____________________________________
+
+	public function getName() {
+		var name = Type.getClassName(Type.getClass(this));
+		return ('${name}');
+	}
+
 	public function toString() {
 		var name = Type.getClassName(Type.getClass(this));
 		return ('${name}: ' + haxe.Json.parse(haxe.Json.stringify(this)));
--- src/IBase.hx
+++ src/IBase.hx
@@ -1,6 +1,8 @@
 interface IBase {
 	public var type:String;
+	@:isVar public var id(get, set):String;
 	public function svg(settings:Settings):String;
 	public function ctx(ctx:js.html.CanvasRenderingContext2D):Void;
 	public function toString():String;
+	public function getName():String;
 }
