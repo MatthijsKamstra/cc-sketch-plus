555ac27e15c6c73340104c72a551f76846979b98
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-08-29 13:10:10 +0200

    lineweight and linecaps added

--- src/draw/Line.hx
+++ src/draw/Line.hx
@@ -9,6 +9,10 @@ class Line extends Base implements IBase {
 
 	@:isVar public var stroke(get, set):String = '#000000';
 
+	@:isVar public var lineCap(get, set):String = 'butt';
+
+	@:isVar public var lineWeight(get, set):Int;
+
 	public var type = 'Line'; // base (get class name?)
 
 	public function new(x, y, x2, y2) {
@@ -25,6 +29,10 @@ class Line extends Base implements IBase {
 		xml.set('x2', Std.string(this.x2));
 		xml.set('y2', Std.string(this.y2));
 		xml.set('stroke', this.stroke);
+		if (lineCap != 'butt')
+			xml.set('stroke-linecap', this.lineCap);
+		if (lineWeight != null)
+			xml.set('stroke-width', Std.string(this.lineWeight));
 		return xml.toString();
 	}
 
@@ -74,5 +82,21 @@ class Line extends Base implements IBase {
 
 	function set_stroke(value:String):String {
 		return stroke = value;
+	}
+
+	function get_lineCap():String {
+		return lineCap;
+	}
+
+	function set_lineCap(value:String):String {
+		return lineCap = value;
+	}
+
+	function get_lineWeight():Int {
+		return lineWeight;
+	}
+
+	function set_lineWeight(value:Int):Int {
+		return lineWeight = value;
 	}
 }
