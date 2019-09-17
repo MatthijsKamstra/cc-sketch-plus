599f38b5c73b8d4a76e66e0bcab78f50ad84037b
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-09-17 11:01:11 +0200

    removed linecap from line and to base

--- src/draw/Line.hx
+++ src/draw/Line.hx
@@ -4,9 +4,6 @@ class Line extends Base implements IBase {
 	@:isVar public var x2(get, set):Int;
 	@:isVar public var y2(get, set):Int;
 
-	// @:isVar public var stroke(get, set):String = '#000000';
-	@:isVar public var lineCap(get, set):String = 'butt';
-
 	@:isVar public var lineWeight(get, set):Int;
 
 	public var type = 'Line'; // base (get class name?)
@@ -25,8 +22,7 @@ class Line extends Base implements IBase {
 		xml.set('x2', Std.string(this.x2));
 		xml.set('y2', Std.string(this.y2));
 		xml.set('stroke', this.stroke);
-		if (lineCap != 'butt')
-			xml.set('stroke-linecap', this.lineCap);
+
 		if (lineWeight != null)
 			xml.set('stroke-width', Std.string(this.lineWeight));
 		return xml.toString();
@@ -54,14 +50,6 @@ class Line extends Base implements IBase {
 
 	function set_y2(value:Int):Int {
 		return y2 = value;
-	}
-
-	function get_lineCap():String {
-		return lineCap;
-	}
-
-	function set_lineCap(value:String):String {
-		return lineCap = value;
 	}
 
 	function get_lineWeight():Int {
