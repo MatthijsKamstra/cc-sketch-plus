9abd0a6d5a1219e39939122e922ec83b6a95955e
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-02-22 16:10:21 +0100

    clear not all, WIP

--- src/html/Container.hx
+++ src/html/Container.hx
@@ -4,19 +4,38 @@ import js.html.Document;
 import js.Browser.*;
 
 class Container {
-	var layout:String;
 	var _id = 'cc-bootstrap-container';
+	var layout:String;
+	var isDebug = false;
 
 	static var _count = 0;
 
-	public function new(str:String) {
+	public function new(?str:String = '', ?isClear:Bool = false) {
 		layout = str;
+		var elems = document.body.getElementsByTagName("*");
+		if (isClear) {
+			// for (i in 0...elems.length) {
+			// 	var _el = elems[i];
+			// 	if (_el == null)
+			// 		return;
+			// 	trace(_el);
+			// 	if (_el.id.toLowerCase().indexOf('cc-') == -1) {
+			// 		_el.parentElement.removeChild(_el);
+			// 	}
+			// }
+
+			document.body.innerHTML = '';
+		}
 		init();
 	}
 
 	function init() {
 		var div = document.createDivElement();
-		div.id = '${_id}-${_count}';
+		if (_count == 0) {
+			div.id = '${_id}';
+		} else {
+			div.id = '${_id}-${_count}';
+		}
 		div.className = 'container';
 		// div.innerHTML = '<!-- test -->';
 		document.body.appendChild(div);
@@ -26,20 +45,17 @@ class Container {
 			var row = _arr[i];
 			var divRow = document.createDivElement();
 			divRow.className = 'row';
-			trace(row);
+			if (isDebug)
+				trace(row);
 			var col:Array<String> = row.split('|');
 			for (i in 0...col.length) {
 				var _col = col[i];
-				trace(_col);
-				if (_col == '') {
-					var divCol = document.createDivElement();
-					divCol.className = 'col';
-					divRow.appendChild(divCol);
-				} else {
-					var divCol = document.createDivElement();
-					divCol.className = 'col';
-					divRow.appendChild(divCol);
-
+				if (isDebug)
+					trace(_col);
+				var divCol = document.createDivElement();
+				divCol.className = 'col';
+				divRow.appendChild(divCol);
+				if (_col != '') {
 					var c = document.createDivElement();
 					c.id = _col;
 					divCol.appendChild(c);
